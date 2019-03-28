403d6b2f7b9cc6f392c45e8a15d38940935e12cc
hebelala <hebelala@qq.com>
2019-03-28 17:04:39 +0800

    use log4j2, enhance zip tasking-container

--- tasking-container/src/assembly/bin/tasking-container.sh
+++ tasking-container/src/assembly/bin/tasking-container.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+PRG="$0"
+PRG_DIR=`dirname "$PRG"`
+BASE_DIR=`cd "$PRG_DIR/.." >/dev/null; pwd`
+
+nohup java -jar ${BASE_DIR}/lib/tasking-container-*.jar >> ${BASE_DIR}/bin/nohup.out 2>&1 &
--- tasking-container/src/main/java/com/github/hebelala/tasking/container/Container.java
+++ tasking-container/src/main/java/com/github/hebelala/tasking/container/Container.java
@@ -1,5 +1,8 @@
 package com.github.hebelala.tasking.container;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,36 +15,40 @@ import java.util.List;
  */
 public class Container {
 
-    private URLClassLoader containerClassLoader;
+    private Logger logger;
+
     private List<URLClassLoader> appClassloaderList = new ArrayList<>();
 
     public void start() throws MalformedURLException {
-        setEnv();
-        initClassloader();
+        File libFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+
+        setEnv(libFile);
+        initLog();
+        initAppClassLoaders(libFile);
     }
 
-    private void setEnv() {
-
+    private void setEnv(File libFile) {
+        File logsFile = new File(libFile.getParentFile().getParent(), "logs");
+        System.setProperty("tasking.log.dir", logsFile.getAbsolutePath());
     }
 
-    private void initClassloader() throws MalformedURLException {
-        String containerPath = Container.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        File containerFile = new File(containerPath);
+    private void initLog() {
+        logger = LoggerFactory.getLogger(getClass());
+    }
 
-        // init containerClassloader
-        List<URL> containerUrlList = new ArrayList<>();
-        loadFiles(containerFile, containerUrlList);
-        containerClassLoader = new URLClassLoader(containerUrlList.toArray(new URL[containerUrlList.size()]));
-
-        // init appClassloaderList
-        File appsFile = new File(containerFile.getParent(), "apps");
-        File[] files = appsFile.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                List<URL> appUrlList = new ArrayList<>();
-                loadFiles(file, appUrlList);
-                appClassloaderList.add(new URLClassLoader(appUrlList.toArray(new URL[appUrlList.size()])));
+    private void initAppClassLoaders(File libFile) throws MalformedURLException {
+        File appsFile = new File(libFile.getParent(), "apps");
+        if (appsFile.exists()) {
+            File[] files = appsFile.listFiles();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    List<URL> appUrlList = new ArrayList<>();
+                    loadFiles(file, appUrlList);
+                    appClassloaderList.add(new URLClassLoader(appUrlList.toArray(new URL[appUrlList.size()])));
+                }
             }
+        } else {
+            logger.info("the apps directory does not exists");
         }
     }
 
