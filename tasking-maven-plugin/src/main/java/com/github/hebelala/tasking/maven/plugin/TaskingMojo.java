acb4ea0f6ba179475789264cb6ff7939463debee
hebelala <hebelala@qq.com>
2019-05-21 15:04:18 +0800

    Fix sonar

--- tasking-maven-plugin/src/main/java/com/github/hebelala/tasking/maven/plugin/TaskingMojo.java
+++ tasking-maven-plugin/src/main/java/com/github/hebelala/tasking/maven/plugin/TaskingMojo.java
@@ -76,16 +76,18 @@ public abstract class TaskingMojo extends AbstractMojo {
 		URLClassLoader appClassLoader = new URLClassLoader(urlList.toArray(new URL[urlList.size()]));
 		try {
 			Enumeration<URL> resources = appClassLoader.findResources("tasking.properties");
-			if (resources == null || !resources.hasMoreElements()) {
-				throw new MojoExecutionException("Cannot find tasking.properties");
-			}
-			int count = 0;
 			URL url = null;
-			while (resources.hasMoreElements()) {
-				if (++count >= 2) {
-					throw new MojoExecutionException("The tasking.properties count cannot exceed 2");
+			if (resources != null) {
+				int count = 0;
+				while (resources.hasMoreElements()) {
+					if (++count >= 2) {
+						throw new MojoExecutionException("The tasking.properties count cannot exceed 2");
+					}
+					url = resources.nextElement();
 				}
-				url = resources.nextElement();
+			}
+			if (url == null) {
+				throw new MojoExecutionException("Cannot find tasking.properties");
 			}
 			InputStream inputStream = url.openStream();
 			try {
