4df7fa54e7cfb81b3e25c6a5e37dc5ce7a637b6a
hebelala <hebelala@qq.com>
2019-07-04 16:59:50 +0800

    add SimpleFutureTaskTest

---
+++ tasking-actor/src/test/java/com/github/hebelala/tasking/actor/task/SimpleFutureTaskTest.java
@@ -0,0 +1,128 @@
+package com.github.hebelala.tasking.actor.task;
+
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class SimpleFutureTaskTest {
+
+	@BeforeAll
+	public static void beforeClass() {
+		System.setProperty("tasking.log.appender", "Console");
+	}
+
+	@Test
+	@Order(1)
+	public void testAwaitFalse() throws InterruptedException {
+		SimpleFutureTask simpleFutureTask = new SimpleFutureTask(new SleepOneSecond());
+		simpleFutureTask.start();
+		assertFalse(simpleFutureTask.await(900L, TimeUnit.MILLISECONDS));
+		simpleFutureTask.shutdown();
+	}
+
+	@Test
+	@Order(2)
+	public void testAwaitTrue() throws InterruptedException {
+		SimpleFutureTask simpleFutureTask = new SimpleFutureTask(new SleepOneSecond());
+		simpleFutureTask.start();
+		assertTrue(simpleFutureTask.await(1100L, TimeUnit.MILLISECONDS));
+		simpleFutureTask.shutdown();
+	}
+
+	@Test
+	@Order(3)
+	public void testShutdown() throws InterruptedException {
+		SleepOneSecond sleepOneSecond = new SleepOneSecond();
+		SimpleFutureTask simpleFutureTask = new SimpleFutureTask(sleepOneSecond);
+		simpleFutureTask.start();
+
+		assertTimeout(ofMillis(1000L), () -> simpleFutureTask.shutdown());
+		assertAll(() -> {
+			Throwable throwable = sleepOneSecond.getThrowable();
+			assertNotNull(throwable);
+			assertTrue(throwable instanceof InterruptedException);
+		});
+	}
+
+	@Test
+	@Order(4)
+	public void testShutdownWithRetrySleep() throws InterruptedException {
+		RetrySleepOneSecond retrySleepOneSecond = new RetrySleepOneSecond();
+		SimpleFutureTask simpleFutureTask = new SimpleFutureTask(retrySleepOneSecond);
+		simpleFutureTask.start();
+
+		assertTimeout(ofMillis(1000L), () -> simpleFutureTask.shutdown());
+		assertAll(() -> {
+			Throwable throwable = retrySleepOneSecond.getThrowable();
+			assertNotNull(throwable);
+			assertTrue(throwable instanceof InterruptedException);
+		});
+		assertAll(() -> {
+			Throwable throwable2 = retrySleepOneSecond.getThrowable2();
+			assertNotNull(throwable2);
+			assertTrue(throwable2 instanceof InterruptedException);
+		});
+	}
+
+	class SleepOneSecond implements Runnable {
+
+		private volatile Throwable throwable;
+
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(1000L);
+			} catch (Throwable t) {
+				throwable = t;
+			}
+		}
+
+		public Throwable getThrowable() {
+			return throwable;
+		}
+
+	}
+
+	class RetrySleepOneSecond implements Runnable {
+
+		private volatile Throwable throwable;
+		private volatile Throwable throwable2;
+
+		@Override
+		public void run() {
+			while (throwable2 == null) {
+				try {
+					Thread.sleep(1000L);
+				} catch (Throwable t) {
+					if (throwable == null) {
+						throwable = t;
+					} else {
+						throwable2 = t;
+					}
+				}
+			}
+		}
+
+		public Throwable getThrowable() {
+			return throwable;
+		}
+
+		public Throwable getThrowable2() {
+			return throwable2;
+		}
+
+	}
+
+}
